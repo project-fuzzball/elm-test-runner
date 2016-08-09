54eb9f98307ec3e5fa3604198b63398b6accc5ca
Richard Feldman <richard.t.feldman@gmail.com>
2016-08-08 23:07:11 -0700

    Deprecate everything.

---
+++ src/Deprecated.elm
@@ -0,0 +1,12 @@
+module Deprecated exposing (deprecated)
+
+{-| DEPRECATED - This has been replaced by rtfeldman/html-test-runner
+@docs deprecated
+-}
+
+
+{-| DEPRECATED - This has been replaced by rtfeldman/html-test-runner
+-}
+deprecated : String
+deprecated =
+    "DEPRECATED - This has been replaced by rtfeldman/html-test-runner"
